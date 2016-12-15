3b453a71516ac3b34fae87b206daab74bd9b5634
Dmytro Kalpakchi <dmytroka@kth.se>
2016-12-15 23:02:09 +0100

    Finished JTA! Yaygit add .git add .

--- jta.py
+++ jta.py
@@ -2,7 +2,7 @@
 # @Author: dmytro
 # @Date:   2016-12-14 14:05:22
 # @Last Modified by:   Dmytro Kalpakchi
-# @Last Modified time: 2016-12-15 22:27:32
+# @Last Modified time: 2016-12-15 23:01:20
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -46,15 +46,21 @@ def get_different(scope1, scope2):
 	return list(set(scope1) - set(scope2))
 
 
-def collect(v, parent):
-	pass
-
-
-def distribute(v):
-	pass
-
-
 def jta(ugm, jtree, evidence):
+	def collect_distribute(order, messages):
+		messaged = []
+		for v in order:
+			for n in jtree.neighbors(v):
+				if n not in messaged:
+					messages[(v, n)] = factors[",".join(v)]
+					for edge, m in messages.iteritems():
+						if edge[1] == v and edge[0] != n:
+							messages[(v, n)] *= m
+					diff = get_different(get_different(v, e), get_different(n, e))
+					messages[(v, n)] = messages[(v, n)].marginalize(diff, inplace=False)
+			messaged.append(v)
+		return messages
+
 	factors = dict([(",".join(f.scope()), f.copy()) for f in jtree.get_factors()])
 	e = map(itemgetter(0), evidence) # evidence scope
 	for f in factors.values():
@@ -62,70 +68,28 @@ def jta(ugm, jtree, evidence):
 		if separator:
 			f.reduce([x for x in evidence if x[0] in separator], inplace=True)
 
-	messages = {}
+	jtree_nodes = jtree.nodes()
+	root = jtree_nodes[0]
 
 	# collect phase
-	collected = []
-	for v in nx.dfs_postorder_nodes(jtree):
-		for n in jtree.neighbors(v):
-			if n not in collected:
-				messages[(v, n)] = factors[",".join(v)]
-				for edge, m in messages.iteritems():
-					if edge[1] == v and edge[0] != n:
-						messages[(v, n)].product(m)
-				diff = get_different(get_different(v, e), get_different(n, e))
-				messages[(v, n)] = messages[(v, n)].marginalize(diff, inplace=False)
-		collected.append(v)
+	messages = collect_distribute(nx.dfs_postorder_nodes(jtree, source=root), {})
 	
 	# distribute phase
-	distributed = []
-	for v in nx.dfs_preorder_nodes(jtree):
-		for n in jtree.neighbors(v):
-			if n not in distributed:
-				messages[(v, n)] = factors[",".join(v)]
-				for edge, m in messages.iteritems():
-					if edge[1] == v and edge[0] != n:
-						messages[(v, n)] *= m
-				diff = get_different(get_different(v, e), get_different(n, e))
-				messages[(v, n)] = messages[(v, n)].marginalize(diff, inplace=False)
-		distributed.append(v)
+	messages = collect_distribute(nx.dfs_preorder_nodes(jtree, source=root), messages)
 
+	# get beliefs for each cluster from messages
 	beliefs = {}
-	for v in jtree.nodes():
+	for v in jtree_nodes:
 		beliefs[v] = factors[",".join(v)]
 		for edge, m in messages.iteritems():
 			if edge[1] == v:
 				beliefs[v] *= m
-
-	for i, m in messages.iteritems():
-		print i, m
-
-	print "================================"
 	
 	marg = {}
 	for m in get_different(ugm.nodes(), e):
-		for v in jtree.nodes():
+		for v in jtree_nodes:
 			if m in v:
 				diff = get_different(get_different(v, e), m)
-				marg[m] = beliefs[v].marginalize(diff, inplace=False)
+				marg[m] = beliefs[v].marginalize(diff, inplace=False).normalize(inplace=False)
 				break
 	return marg.itervalues()
-
-
-V, E, CPD = readAdjList("./student_adjlist_dgm.csv")
-
-DGM = BayesianModel()
-DGM.add_nodes_from(V)
-DGM.add_edges_from(E)
-DGM.add_cpds(*CPD)
-
-UGM = DGM.to_markov_model()
-jtree = UGM.to_junction_tree()
-
-marginal = jta(UGM, jtree, [('I', 1)])
-for m in marginal:
-	print m
-
-# nx.draw(jtree, with_labels=True, node_color="white", node_shape='s', node_size=8000)
-# plt.draw()
-# plt.show()
---
+++ jta_test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# @Author: Dmytro Kalpakchi
+# @Date:   2016-12-15 22:38:36
+# @Last Modified by:   Dmytro Kalpakchi
+# @Last Modified time: 2016-12-15 23:01:52
+
+from pgmpy.inference import VariableElimination
+from jta import readAdjList, jta, BayesianModel, get_different
+
+if __name__ == '__main__':
+	V, E, CPD = readAdjList("./student_adjlist_dgm.csv")
+
+	print "Results of the implemented JTA"
+
+	DGM = BayesianModel()
+	DGM.add_nodes_from(V)
+	DGM.add_edges_from(E)
+	DGM.add_cpds(*CPD)
+
+	UGM = DGM.to_markov_model()
+	jtree = UGM.to_junction_tree()
+
+	evidence = {}
+
+	marginal = jta(UGM, jtree, evidence.items())
+	for m in marginal:
+		print m
+
+	print "\n=======================================\n"
+
+	print "Results of the Variable Elimination from pgmpy"
+
+	inference = VariableElimination(DGM)
+	for v in get_different(DGM.nodes(), evidence):
+		print inference.query(variables=[v], evidence=evidence)[v]
+
+	# visualization part
+	# nx.draw(jtree, with_labels=True, node_color="white", node_shape='s', node_size=8000)
+	# plt.draw()
+	# plt.show()
