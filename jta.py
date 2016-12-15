6bdf368aba2f63c013807915ab57d316b60adf5b
Dmytro Kalpakchi <dmytroka@kth.se>
2016-12-15 19:14:57 +0100

    Started JTA, incorporated evidence into the factors

--- jta.py
+++ jta.py
@@ -2,14 +2,15 @@
 # @Author: dmytro
 # @Date:   2016-12-14 14:05:22
 # @Last Modified by:   Dmytro Kalpakchi
-# @Last Modified time: 2016-12-15 10:09:04
+# @Last Modified time: 2016-12-15 19:10:14
 
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import csv
 from pgmpy.models import *
-from pgmpy.factors import Factor, TabularCPD
+from pgmpy.factors.discrete import TabularCPD
+from operator import itemgetter
 
 
 def readAdjList(file):
@@ -31,28 +32,38 @@ def readAdjList(file):
 			depend = row[0].split("|")
 			var = depend[0]
 			evidence = depend[1].split(".") if len(depend) > 1 else []
-			card[var] = int(row[1])
-			cpd = [map(float, x.split("|")) for x in row[2].split("||")]
+			card[var] = np.int(row[1])
+			cpd = [map(np.float, x.split("|")) for x in row[2].split("||")]
 			cpds.append(TabularCPD(var, card[var], cpd, evidence=evidence, evidence_card=[card[e] for e in evidence]))
 	return vertices, edges, cpds
 
 
+def get_separator(scope1, scope2):
+	return list(set(scope1) & set(scope2))
+
+
+def jta(jtree, evidence):
+	factors = jtree.get_factors()
+	evidence_scope = map(itemgetter(0), evidence)
+	for f in factors:
+		separator = get_separator(evidence_scope, f.scope())
+		if separator:
+			f.reduce([x for x in evidence if x[0] in separator], inplace=True)
+	
+
+
 V, E, CPD = readAdjList("./student_adjlist_dgm.csv")
 
-# G = BayesianModel()
-# G.add_nodes_from(V)
-# G.add_edges_from(E)
-# nx.draw(G, with_labels=True, node_color="white")
+DGM = BayesianModel()
+DGM.add_nodes_from(V)
+DGM.add_edges_from(E)
+DGM.add_cpds(*CPD)
 
-# G = MarkovModel()
-# G.add_nodes_from(V)
-# G.add_edges_from(E)
-# phi = [Factor(edge, [2, 2], np.random.rand(4)) for edge in G.edges()]
-# G.add_factors(*phi)
-# jtree = G.to_junction_tree()
-# nx.draw(G, with_labels=True, node_color="white")
-# plt.draw()
-# plt.show()
+UGM = DGM.to_markov_model()
+jtree = UGM.to_junction_tree()
+
+jta(jtree, [('I', 1)])
+
 # nx.draw(jtree, with_labels=True, node_color="white", node_shape='s', node_size=8000)
 # plt.draw()
 # plt.show()
